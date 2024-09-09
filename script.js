13053a33cf8af02e9123b85bf400524ac754953e
eurus <evr@gmx.us>
2024-09-09 17:09:32 +0500

    respawn

--- script.js
+++ script.js
@@ -1,63 +1,62 @@
-(function () {
-  $(".skills-prog li")
-    .find(".skills-bar")
-    .each(function (i) {
-      $(this)
-        .find(".bar")
-        .delay(i * 150)
-        .animate(
-          {
-            width: $(this).parents().attr("data-percent") + "%",
-          },
-          1000,
-          "linear",
-          function () {
-            return $(this).css({
-              "transition-duration": ".5s",
-            });
-          }
-        );
-    });
-
-  $(".skills-soft li")
-    .find("svg")
-    .each(function (i) {
-      var c, cbar, circle, percent, r;
-      circle = $(this).children(".cbar");
-      r = circle.attr("r");
-      c = Math.PI * (r * 2);
-      percent = $(this).parent().data("percent");
-      cbar = ((100 - percent) / 100) * c;
-      circle.css({
-        "stroke-dashoffset": c,
-        "stroke-dasharray": c,
-      });
-      circle.delay(i * 150).animate(
+/* Count to a specific number */
+function count(els) {
+  els.each(function () {
+    $(this)
+      .prop("Counter", 0)
+      .animate(
         {
-          strokeDashoffset: cbar,
+          Counter: $(this).text(),
         },
-        1000,
-        "linear",
-        function () {
-          return circle.css({
-            "transition-duration": ".3s",
-          });
+        {
+          duration: 3000,
+          easing: "swing",
+          step: function (now) {
+            $(this).text(Math.ceil(now));
+          },
         }
       );
-      $(this)
-        .siblings("small")
-        .prop("Counter", 0)
-        .delay(i * 150)
-        .animate(
-          {
-            Counter: percent,
-          },
-          {
-            duration: 1000,
-            step: function (now) {
-              return $(this).text(Math.ceil(now) + "%");
-            },
-          }
-        );
-    });
-}).call(this);
+  });
+}
+/* Count when the counter section is in viewport */
+const counterEls = $(".counter .counter__count");
+new Waypoint({
+  element: counterEls,
+
+  handler: function () {
+    count(counterEls);
+
+    this.destroy();
+  },
+
+  offset: "bottom-in-view",
+});
+
+/* Make navbar fixed when scrolled down and hide it when scrolled up */
+const navbar = $(".navbar-top");
+let previousScroll = 0;
+$(window).on("scroll", function (event) {
+  let currentScroll = $(window).scrollTop(); //Distance scrolled down the page
+  let navHeight = $(navbar).height(); //Height of navbar
+  if (currentScroll > 10) {
+    $(".navbar-top").addClass("navbar-top--scrolled");
+  } else {
+    $(".navbar-top").removeClass("navbar-top--scrolled");
+  }
+
+  //When scrolling down AND you've scrolled past navHeight * 2.25, add .scrollUp
+  if (currentScroll > previousScroll && currentScroll > navHeight * 2.25) {
+    $(navbar).addClass("scrollUp");
+    //When scrolling up AND you've scrolled less than navHeight, remove .scrollUp
+  } else if (previousScroll > currentScroll && !(currentScroll <= navHeight)) {
+    $(navbar).removeClass("scrollUp");
+  }
+  previousScroll = currentScroll;
+});
+
+/* Add navbar background color when it is not collapsed */
+$("#navbarTopCollapsible").on("show.bs.collapse", function () {
+  $(".navbar-top").addClass("bg-dark-trans");
+});
+$("#navbarTopCollapsible").on("hide.bs.collapse", function () {
+  $(".navbar-top").removeClass("bg-dark-trans");
+});
